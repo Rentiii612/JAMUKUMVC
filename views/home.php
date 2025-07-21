09524cf9fa0e88560d7fa7157b78d4ccd55fd93a
Rentiii612 <yorentianarafu@gmail.com>
2025-07-22 00:26:08 +0700

    Menambahkan file home di folder views MVCV-JAMUKU

--- views/home.php
+++ views/home.php
@@ -1,3 +1,3 @@
-<h1>Selamat Datang di Aplikasi Jamuku!</h1>
- <a href="/bahan">Mulai Pilih Bahan Jamu</a>
+<h1>Selamat Datang di Aplikasi Jamunya Yoren!</h1>
+ <a href="/bahan">Nih dipilih bahan jamunya</a>
 
